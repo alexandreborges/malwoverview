fbb078eac23aabfc37498af8e150c6da35eb398e
Alexandre Borges <alexandreborges@blackstormsecurity.com>
2023-08-07 16:19:03 -0300

    Malwoverview

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "pefile",
         "colorama",
-        "python-magic; platform_system == 'Linux' and 'Darwin'",
+        "python-magic; platform_system == ('Linux','Darwin')",
         "simplejson",
         "requests",
         "validators",
